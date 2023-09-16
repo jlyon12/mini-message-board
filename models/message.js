8baf635529a75b3fba61a876e59a7b8d283d2304
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-15 18:35:05 -0600

    update Message model
      add timestamp key

--- models/message.js
+++ models/message.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
 	text: { type: String, required: true },
 	sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	timestamp: { type: Date, default: Date.now() },
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
