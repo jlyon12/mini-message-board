49405bb1922c589a0e48ea31f5c9300f521dfdcd
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-15 22:51:46 -0600

    fix date bug - message Timestamp
      use new Date() to generate a new date instance when the message is
      created

--- models/message.js
+++ models/message.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
 	text: { type: String, required: true },
 	sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-	timestamp: { type: Date, default: Date.now() },
+	timestamp: { type: Date, default: new Date() },
 });
 
 MessageSchema.virtual('timestamp_formatted').get(function () {
