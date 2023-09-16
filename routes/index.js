98bb6c1bc338c186dcf4ad99b5da65b9cc2d6c34
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-15 20:21:03 -0600

    Render messages on index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const message_controller = require('../controllers/messageController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', message_controller.message_list);
 
 module.exports = router;
